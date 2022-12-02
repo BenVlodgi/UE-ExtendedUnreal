dda3da1cef1df20b44a0f292ac05b63bf6c435b3
Benjamin Thomas Blodgett <benvlodgi@gmail.com>
2022-12-02 13:23:33 -0500

    Add base plugin

---
+++ Source/ExtendedUnreal/ExtendedUnreal.Build.cs
@@ -0,0 +1,54 @@
+// Some copyright should be here...
+
+using UnrealBuildTool;
+
+public class ExtendedUnreal : ModuleRules
+{
+	public ExtendedUnreal(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = ModuleRules.PCHUsageMode.UseExplicitOrSharedPCHs;
+		
+		PublicIncludePaths.AddRange(
+			new string[] {
+				// ... add public include paths required here ...
+			}
+			);
+				
+		
+		PrivateIncludePaths.AddRange(
+			new string[] {
+				// ... add other private include paths required here ...
+			}
+			);
+			
+		
+		PublicDependencyModuleNames.AddRange(
+			new string[]
+			{
+				"Core",
+				"GameplayTags", // I believe this needs to be public, because I am using the FGameplayTag struct and cannot forward-declare it.
+				// ... add other public dependencies that you statically link with here ...
+			}
+			);
+			
+		
+		PrivateDependencyModuleNames.AddRange(
+			new string[]
+			{
+				"CoreUObject",
+				"Engine",
+				"Slate",
+				"SlateCore",
+				// ... add private dependencies that you statically link with here ...	
+			}
+			);
+		
+		
+		DynamicallyLoadedModuleNames.AddRange(
+			new string[]
+			{
+				// ... add any modules that your module loads dynamically here ...
+			}
+			);
+	}
+}
